506a394c5af7c52e9b533b7dab8c920786cdde1b
Anirudh Jakhar <anjakhar2000@gmail.com>
2018-04-17 12:36:34 +0530

    Add files via upload

---
+++ BuildABasicPHPWebsite/catalog.php
@@ -0,0 +1,50 @@
+<?php 
+
+include("includes/data.php");
+include("includes/functions.php");
+
+$pageTitle = "Full Catalog";
+
+$section = null;
+
+if (isset($_GET["cat"])){
+    if($_GET["cat"] == "books")
+    {
+        $pageTitle = "Books";
+        $section = "Books";
+    }
+    else if ($_GET["cat"] == "movies"){
+        $pageTitle = "Movies";
+        $section = "Movies";
+    }
+    else if ($_GET["cat"] == "music"){
+        $pageTitle = "Music";
+        $section = "Music";
+    }
+}
+
+include("includes/header.php");?>
+
+<div class="section catalog page">
+    <div class="wrapper">
+        <h1><?php 
+            
+            if ($section != null){
+                echo "<a href='catalog.php'>Full Catalog</a> &gt;";
+            }
+            echo $pageTitle; ?></h1>
+        
+        <ul class="items">
+            <?php 
+            $categories = array_category($catalog,$section);
+            
+            foreach ($categories as $id) {
+                echo get_item_html($id,$catalog[$id]);
+            }
+            ?>
+        </ul>
+    </div>
+    
+</div>
+
+<?php include("includes/footer.php"); ?>
